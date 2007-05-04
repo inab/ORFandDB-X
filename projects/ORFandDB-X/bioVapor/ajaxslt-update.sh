c093cff3a54e4d61b187dac292db016d355eca26
José María Fernández <jmfernandez@cnio.es>
2007-05-04 14:17:32 +0000

    *	More code reorganization. *	Absolute path for OMIM collection inside modules. *	Due code reorganization, update scripts have been changed.
    git-svn-id: http://trac.bioinfo.cnio.es/svn/ORFandDB-X/trunk@97 756264e7-042e-4c5a-b60f-73da9b56da12

--- projects/ORFandDB-X/bioVapor/ajaxslt-update.sh
+++ projects/ORFandDB-X/bioVapor/ajaxslt-update.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+THEBASEDIR=$(dirname "$0")
+
+case "$THEBASEDIR" in
+	/*)
+		;;
+	*)
+		THEBASEDIR="${PWD}/${THEBASEDIR}"
+		;;
+esac
+
+LOCALAJAXSLT="$THEBASEDIR"/widget/ajaxslt
+
 # Getting the latest copy!
-rm -rf ajaxslt
-svn update ajaxslt
+rm -rf "$LOCALAJAXSLT"
+svn update "$LOCALAJAXSLT"
 
 if [ ! -d ajaxslt-svn ] ; then
 	svn checkout http://ajaxslt.googlecode.com/svn/trunk/ ajaxslt-svn
@@ -13,15 +25,15 @@ fi
 rm -rf ajaxslt-snapshot
 cp -dprf ajaxslt-svn ajaxslt-snapshot
 find ajaxslt-snapshot -name .svn -exec rm -rf {} \; >& /dev/null
-find ajaxslt-snapshot -type f -name .svnignore -exec rm -rf {} \;
-svn -R list ajaxslt | sed 's#/$##g' | sort > ajaxslt-old.ls-R
-find ajaxslt-snapshot | tail -n +2 | cut -d / -f 2- | sort > ajaxslt-snapshot.ls-R
-diff ajaxslt-old.ls-R ajaxslt-snapshot.ls-R > ajaxslt-ls-R.diff
-cp -dprf ajaxslt-snapshot/* ajaxslt-snapshot/.[a-zA-Z]* ajaxslt
+find ajaxslt-snapshot -type f -name .svnignore -exec rm -rf {} \; >& /dev/null
+svn -R list ajaxslt | sed 's#/$##g' | sort > "$THEBASEDIR"/ajaxslt-old.ls-R
+find ajaxslt-snapshot | tail -n +2 | cut -d / -f 2- | sort > "$THEBASEDIR"/ajaxslt-snapshot.ls-R
+diff "$THEBASEDIR"/ajaxslt-old.ls-R "$THEBASEDIR"/ajaxslt-snapshot.ls-R > "$THEBASEDIR"/ajaxslt-ls-R.diff
+cp -dprf ajaxslt-snapshot/* ajaxslt-snapshot/.[a-zA-Z]* "$LOCALAJAXSLT"
 
 cd ajaxslt
-addedfiles=$(grep '^> ' ../ajaxslt-ls-R.diff | cut -c 3-)
-erasedfiles=$(grep '^< ' ../ajaxslt-ls-R.diff | cut -c 3-)
+addedfiles=$(grep '^> ' "$THEBASEDIR"/ajaxslt-ls-R.diff | cut -c 3-)
+erasedfiles=$(grep '^< ' "$THEBASEDIR"/ajaxslt-ls-R.diff | cut -c 3-)
 
 echo "Files to add:"
 echo "$addedfiles"
@@ -31,11 +43,11 @@ echo "$erasedfiles"
 echo
 
 if [ -n "$addedfiles" ] ; then
-	svn add $(grep '^> ' ../ajaxslt-ls-R.diff | cut -c 3-)
+	svn add $(grep '^> ' "$THEBASEDIR"/ajaxslt-ls-R.diff | cut -c 3-)
 fi
 if [ -n "$erasedfiles" ] ; then
-	svn remove $(grep '^< ' ../ajaxslt-ls-R.diff | cut -c 3-)
+	svn remove $(grep '^< ' "$THEBASEDIR"/ajaxslt-ls-R.diff | cut -c 3-)
 fi
 svn commit
-cd ..
+cd "$THEBASEDIR"
 rm -rf ajaxslt-snapshot ajaxslt-old.ls-R ajaxslt-snapshot.ls-R ajaxslt-ls-R.diff
--- projects/ORFandDB-X/bioVapor/sarissa-update.sh
+++ projects/ORFandDB-X/bioVapor/sarissa-update.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+THEBASEDIR=$(dirname "$0")
+
+case "$THEBASEDIR" in
+	/*)
+		;;
+	*)
+		THEBASEDIR="${PWD}/${THEBASEDIR}"
+		;;
+esac
+
+LOCALSARISSA="$THEBASEDIR"/widget/sarissa
+
 # Getting the latest copy!
-rm -rf sarissa
-svn update sarissa
+rm -rf "$LOCALSARISSA"
+svn update "$LOCALSARISSA"
 
 if [ ! -d sarissa-cvs ] ; then
 	cvs -d:pserver:anonymous@sarissa.cvs.sourceforge.net:/cvsroot/sarissa login <<EOF
@@ -18,15 +30,15 @@ fi
 rm -rf sarissa-snapshot
 cp -dprf sarissa-cvs sarissa-snapshot
 find sarissa-snapshot -name CVS -exec rm -rf {} \; >& /dev/null
-find sarissa-snapshot -type f -name .cvsignore -exec rm -rf {} \;
-svn -R list sarissa | sed 's#/$##g' | sort > sarissa-old.ls-R
-find sarissa-snapshot | tail -n +2 | cut -d / -f 2- | sort > sarissa-snapshot.ls-R
-diff sarissa-old.ls-R sarissa-snapshot.ls-R > sarissa-ls-R.diff
-cp -dprf sarissa-snapshot/* sarissa-snapshot/.[a-zA-Z]* sarissa
+find sarissa-snapshot -type f -name .cvsignore -exec rm -rf {} \; >& /dev/null
+svn -R list "$LOCALSARISSA" | sed 's#/$##g' | sort > "${THEBASEDIR}"/sarissa-old.ls-R
+find sarissa-snapshot | tail -n +2 | cut -d / -f 2- | sort > "${THEBASEDIR}"/sarissa-snapshot.ls-R
+diff "${THEBASEDIR}"/sarissa-old.ls-R "${THEBASEDIR}"/sarissa-snapshot.ls-R > "${THEBASEDIR}"/sarissa-ls-R.diff
+cp -dprf sarissa-snapshot/* sarissa-snapshot/.[a-zA-Z]* "$LOCALSARISSA"
 
-cd sarissa
-addedfiles=$(grep '^> ' ../sarissa-ls-R.diff | cut -c 3-)
-erasedfiles=$(grep '^< ' ../sarissa-ls-R.diff | cut -c 3-)
+cd "$LOCALSARISSA"
+addedfiles=$(grep '^> ' "$THEBASEDIR"/sarissa-ls-R.diff | cut -c 3-)
+erasedfiles=$(grep '^< ' "$THEBASEDIR"/sarissa-ls-R.diff | cut -c 3-)
 
 echo "Files to add:"
 echo "$addedfiles"
@@ -36,11 +48,11 @@ echo "$erasedfiles"
 echo
 
 if [ -n "$addedfiles" ] ; then
-	svn add $(grep '^> ' ../sarissa-ls-R.diff | cut -c 3-)
+	svn add $(grep '^> ' "$THEBASEDIR"/sarissa-ls-R.diff | cut -c 3-)
 fi
 if [ -n "$erasedfiles" ] ; then
-	svn remove $(grep '^< ' ../sarissa-ls-R.diff | cut -c 3-)
+	svn remove $(grep '^< ' "$THEBASEDIR"/sarissa-ls-R.diff | cut -c 3-)
 fi
 svn commit
-cd ..
+cd "$THEBASEDIR"
 rm -rf sarissa-snapshot sarissa-old.ls-R sarissa-snapshot.ls-R sarissa-ls-R.diff
